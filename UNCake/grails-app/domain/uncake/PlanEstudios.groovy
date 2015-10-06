736c6b293cda50e1b2053ffc82147297f460e6b3
Santiago <sahernandezbo@unal.edu.co>
2015-10-06 02:50:25 -0500

    Clase PlanEstudios creada

---
+++ UNCake/grails-app/domain/uncake/PlanEstudios.groovy
@@ -0,0 +1,13 @@
+package uncake
+
+class PlanEstudios {
+    Integer codigo
+    String facultad
+    String Departamento
+    //materias
+    Integer creditos_fundamentacion
+    Integer creditos_libre_eleccion
+    Integer creditos_disciplinar
+    static constraints = {
+    }
+}
