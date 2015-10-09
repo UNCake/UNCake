e7c22e37f96f18d04de242a1ad6ecd8fa05191c2
Santiago <sahernandezbo@unal.edu.co>
2015-10-09 00:49:52 -0500

    typo arreglado

--- UNCake/grails-app/domain/uncake/User.groovy
+++ UNCake/grails-app/domain/uncake/User.groovy
@@ -2,7 +2,7 @@ package uncake
 
 class User {
     String name
-    Integer codr
+    Integer code
     Integer program_code
     static belongsTo = [timeline:Timeline]
     static hasMany = [course:Course]
