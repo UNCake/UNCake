56e4daef6e26460843319ecda539a12e144a5e3e
Santiago <sahernandezbo@unal.edu.co>
2015-10-09 00:03:22 -0500

    Course and Schedule refactored

--- UNCake/grails-app/domain/uncake/Course.groovy
+++ UNCake/grails-app/domain/uncake/Course.groovy
@@ -3,7 +3,7 @@ package uncake
 class Course {
     Integer code
     Integer credits
-    static hasMany = [groups: Grupo]
+    static hasMany = [groups: Group]
     static mappedBy = [groups: "none"]
     Integer course_id
     String name
--- UNCake/src/test/groovy/uncake/ScheduleSpec.groovy
+++ UNCake/src/test/groovy/uncake/ScheduleSpec.groovy
@@ -6,8 +6,8 @@ import spock.lang.Specification
 /**
  * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
  */
-@TestFor(Grupo)
-class GrupoSpec extends Specification {
+@TestFor(Schedule)
+class ScheduleSpec extends Specification {
 
     def setup() {
     }
