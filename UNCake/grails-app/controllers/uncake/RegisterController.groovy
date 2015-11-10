9e486ae2c365b86ce1bf27f9334257c84212bf4c
santicoxd <scarvajalc@unal.edu.co>
2015-11-10 14:58:57 -0500

    Seguridad en  falsificacion de form submission , cifrado de paswwords en la base de datos, fix de enlaces en vista de login y register

--- UNCake/grails-app/controllers/uncake/RegisterController.groovy
+++ UNCake/grails-app/controllers/uncake/RegisterController.groovy
@@ -12,7 +12,7 @@ class RegisterController {
             flash.message1="El email ya existe"
             redirect(controller:'register',action:'index')
         }else{
-            if(new User(email: params.email, password: params.pwd2, name: params.nombre).save()==null){
+            if(new User(email: params.email, password: params.pwd2.encodeAsSHA1(), name: params.nombre).save()==null){
                 flash.message1="El email es invalido"
                 redirect(controller:'register',action:'index')
 
