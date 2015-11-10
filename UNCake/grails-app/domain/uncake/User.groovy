ba2ad2b90fef8c9e14b9de86c446cf146396a041
santicoxd <scarvajalc@unal.edu.co>
2015-11-10 08:27:42 -0500

    Revert "Clase usuario adpatada al plugin de seguridad"
    This reverts commit 0a44444e75ae365ed44a553aa557b2365347114b.

--- UNCake/grails-app/domain/uncake/User.groovy
+++ UNCake/grails-app/domain/uncake/User.groovy
@@ -2,66 +2,11 @@ package uncake
 
 class User {
 
-    private static final long serialVersionUID = 1
-
-    transient springSecurityService
-
     String name
     String email
     String password
     //Posible avatar Img avt
-
-    //SS
-    boolean enabled = true
-    boolean accountExpired
-    boolean accountLocked
-    boolean passwordExpired
-
-
     static hasMany = [academicRecord: AcademicRecord, friends: User, schedules: Schedule]
-
-
-    //SS
-    @Override
-    int hashCode() {
-        username?.hashCode() ?: 0
-    }
-
-    @Override
-    boolean equals(other) {
-        is(other) || (other instanceof User && other.username == username)
-    }
-
-    @Override
-    String toString() {
-        username
-    }
-
-    Set<Role> getAuthorities() {
-        UserRole.findAllByUser(this)*.role
-    }
-
-    def beforeInsert() {
-        encodePassword()
-    }
-
-    def beforeUpdate() {
-        if (isDirty('password')) {
-            encodePassword()
-        }
-    }
-
-    protected void encodePassword() {
-        password = springSecurityService?.passwordEncoder ? springSecurityService.encodePassword(password) : password
-    }
-
-    static transients = ['springSecurityService']
-
-    static mapping = {
-        password column: '`password`'
-    }
-
-
     static constraints = {
         name blank: false
         email blank: false, email: true
