9aba66fd52b4dca9a2d658053a44b2f3078f1163
anmrdz <anmrdz@gmail.com>
2015-11-10 22:46:31 -0500

    nota agregada y eliminacion de lista de StudyPlan

--- UNCake/grails-app/domain/uncake/Course.groovy
+++ UNCake/grails-app/domain/uncake/Course.groovy
@@ -7,9 +7,15 @@ class Course {
     static hasMany = [groups: Groups]
     String name
     String typology
+    Double grade
+    String semester
     static constraints = {
         code nullable: false, unique: true
         name blank: false
         typology blank: false
+        grade nullable: true
+        credits nullable: true
+        semester nullable: true
+        groups nullable: true
     }
 }
