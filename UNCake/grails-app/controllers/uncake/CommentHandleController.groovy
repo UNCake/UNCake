0a38b4dd5045eec1adf38640285dd4f32948c6c2
Santiago <sahernandezbo@unal.edu.co>
2015-11-26 05:30:52 -0500

    para hacer pull

--- UNCake/grails-app/controllers/uncake/CommentHandleController.groovy
+++ UNCake/grails-app/controllers/uncake/CommentHandleController.groovy
@@ -4,21 +4,21 @@ package uncake
 //BORRAR LOS PRINTLN DESPUES
 class CommentHandleController {
     def index() {
-        println("index")
+        //println("index")
     }
 
     def comments() {
-        println("in comments")
-        println(params.code)
+        //println("in comments")
+        //println(params.code)
         def course = Course.findByCode(params.code)
         render(view: "comments", model:[name: course.name, comments: course.comments, code: course.code])
     }
 
 
     def saveComment() {
-        println("in save")
-        println(params.comment)
-        println("code: "+params.code)
+        //println("in save")
+        //println(params.comment)
+        //println("code: "+params.code)
         def comment = new Comment(comment: params.comment)
         comment.save()
         def course = Course.findByCode(params.code)
@@ -26,7 +26,5 @@ class CommentHandleController {
 
         render ''
     }
-    def not_fun() {
-        render 'shet'
-    }
+
 }
